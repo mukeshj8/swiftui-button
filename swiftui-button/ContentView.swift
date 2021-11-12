4ba02b62e3a30f922b5ab8376b3edd865ab41874
Mukesh <mukeshxain@gmail.com>
2021-11-12 11:44:36 +0530

    project initial source code

---
+++ swiftui-button/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-button
+//
+//  Created by Mukesh Jain on 12/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-button/swiftui_buttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_buttonApp.swift
+//  swiftui-button
+//
+//  Created by Mukesh Jain on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_buttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
